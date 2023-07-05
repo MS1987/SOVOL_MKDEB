d7f2a91d3778333f299a76a5b5778ddd39e7c77f
Joyce-lujunxu <1130956035@qq.com>
2023-07-05 20:14:08 +0800

    LED 界面添加滑动控制模块

--- home/mks/KlipperScreen/panels/led.py
+++ home/mks/KlipperScreen/panels/led.py
@@ -10,56 +10,95 @@ from ks_includes.screen_panel import ScreenPanel
 
 
 def create_panel(*args):
-	return LedPanel(*args)
+    return LEDPanel(*args)
 
 
-class LedPanel(ScreenPanel):
-	def __init__(self, screen, title):
-		super().__init__(screen, title)
-		self.fan_speed = {}
-		self.devices = {}
-		# Create a grid for all devices
-		self.labels['devices'] = Gtk.Grid()
-		self.labels['devices'].set_valign(Gtk.Align.CENTER)
+class LEDPanel(ScreenPanel):
+    def __init__(self, screen, title):
+        super().__init__(screen, title)
+        self.led_speed = {}
+        self.devices = {}
+        # Create a grid for all devices
+        self.labels['devices'] = Gtk.Grid()
+        self.labels['devices'].set_valign(Gtk.Align.CENTER)
+        self.scale = None
 
-		self.add_led()
+        self.add_led()
 
-		scroll = self._gtk.ScrolledWindow()
-		scroll.add(self.labels['devices'])
+        scroll = self._gtk.ScrolledWindow()
+        scroll.add(self.labels['devices'])
 
-		self.content.add(scroll)
+        self.content.add(scroll)
 
-	def add_led(self):
-		name = Gtk.Label()
-		name.set_markup(f"\n<big><b>Extruder LED</b></big>\n")
-		name.set_hexpand(True)
-		name.set_vexpand(True)
-		name.set_halign(Gtk.Align.START)
-		name.set_valign(Gtk.Align.CENTER)
-		name.set_line_wrap(True)
-		name.set_line_wrap_mode(Pango.WrapMode.WORD_CHAR)
+    def process_update(self, action, data):
+        if action != "notify_status_update":
+            return
 
-		led_col = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=5)
+        for led in self.devices:
+            if led in data and "speed" in data[led]:
+                self.update_led_speed(None, led, self._printer.get_led_speed(led))
 
-		led_off = self._gtk.Button("led-off", _("led off"), "color1")
-		led_off.set_hexpand(False)
-		led_off.connect("clicked", self.set_led, "SET_PIN PIN=my_led VALUE=0")
+    def update_led_speed(self, widget, led, speed):
+        if led not in self.devices:
+            return
 
-		led_on = self._gtk.Button("led-on", _("led on"), "color2")
-		led_on.set_hexpand(False)
-		led_on.connect("clicked", self.set_led, "SET_PIN PIN=my_led VALUE=1")
+        if self.devices[led]['changeable'] is True:
+            if self.devices[led]['scale'].has_grab():
+                return
+            self.devices[led]["speed"] = round(float(speed) * 100)
+            self.devices[led]['scale'].disconnect_by_func(self.set_led_speed)
+            self.devices[led]['scale'].set_value(self.devices[led]["speed"])
+            self.devices[led]['scale'].connect("button-release-event", self.set_led_speed, led)
+        else:
+            self.devices[led]["speed"] = float(speed)
+            self.devices[led]['scale'].set_fraction(self.devices[led]["speed"])
+        if widget is not None:
+            self.set_led_speed(None, None, led)
 
-		led_col.add(name)
-		led_col.add(led_off)
-		led_col.add(led_on)
+    def add_led(self):
+        name = Gtk.Label()
+        name.set_markup(f"\n<big><b>Extruder LED</b></big>\n")
+        name.set_hexpand(True)
+        name.set_vexpand(True)
+        name.set_halign(Gtk.Align.START)
+        name.set_valign(Gtk.Align.CENTER)
+        name.set_line_wrap(True)
+        name.set_line_wrap_mode(Pango.WrapMode.WORD_CHAR)
+        led_col = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=5)
+        led_off = self._gtk.Button("led_off", _("led off"), "color1")
+        led_off.set_hexpand(False)
+        led_off.connect("clicked", self.set_led_speed, "SET_PIN PIN=my_led VALUE=0")
+        led_on = self._gtk.Button("led_on", _("led on"), "color2")
+        led_on.set_hexpand(False)
+        led_on.connect("clicked", self.set_led_speed, "SET_PIN PIN=my_led VALUE=1")
 
-		# led_row = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-		# led_row.add(name)
-		# led_row.add(led_col)
+        self.labels['devices'].insert_row(0)
+        self.labels['devices'].attach(led_col, 0, 0, 1, 1)
+        self.labels['devices'].show_all()
 
-		self.labels['devices'].insert_row(0)
-		self.labels['devices'].attach(led_col, 0, 0, 1, 1)
-		self.labels['devices'].show_all()
+        self.scale = Gtk.Scale.new_with_range(orientation=Gtk.Orientation.HORIZONTAL, min=0, max=100, step=1)
+        self.scale.set_value(0)
+        self.scale.set_digits(0)
+        self.scale.set_hexpand(True)
+        self.scale.set_has_origin(True)
+        self.scale.get_style_context().add_class("led_slider")
+        self.scale.connect("button-release-event", self.set_led_speed)
 
-	def set_led(self, widget, led_status):
-		self._screen._ws.klippy.gcode_script(led_status)
+        led_col.add(led_off)
+        led_col.add(self.scale)
+        led_col.add(led_on)
+
+        led_row = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        led_row.add(name)
+        led_row.add(led_col)
+
+        self.labels['devices'].insert_row(0)
+        self.labels['devices'].attach(led_row, 0, 0, 1, 1)
+        self.labels['devices'].show_all()
+
+    def set_led_speed(self, widget, command):
+        print(f"command: {command}, {self.scale.get_value()}")
+        if type(command) == str:
+            self._screen._ws.klippy.gcode_script(command)
+        else:
+            self._screen._ws.klippy.gcode_script(f"SET_PIN PIN=my_led VALUE={float(self.scale.get_value()) / 100}")
